6b86f2095cb270ce762ee0bee2813a7b1899a82e
Alekhya Thandlay <alekhya_thandlay@comcast.com>
2020-03-11 15:08:03 -0400

    initial commit

---
+++ app/src/main/java/com/example/motioneditor_test/MathActivity.kt
@@ -0,0 +1,16 @@
+package com.example.motioneditor_test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_04.*
+import kotlinx.android.synthetic.main.activity_art.*
+
+class MathActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sample)
+    }
+}
